14191a34cc4ee0fc130ede28358b51cc16c55d1d
Jonas Eckerström <jonas.eckerstrom@softhouse.se>
2015-09-07 14:08:43 +0200

    openssl generated keys don't work by keystore ones do

--- src/main/java/echoClientServer/EchoClient.java
+++ src/main/java/echoClientServer/EchoClient.java
@@ -1,38 +1,94 @@
 package echoClientServer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Field;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
-import java.io.*;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
-public
-class EchoClient {
-    public
-            static
-    void
-            main(String[] arstring) {
-        try {
-            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            //SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 9999);
+public class EchoClient {
+	private static final String JAVA_KEYSTORE_INSTANCE_KEY = "JKS";
+	private static final String JKS_PATH = "keys/keystore.jks";
+	private static final String KEYSTORE_PASSWORD = "keystorePassword";
+	private static final String SSL_CONTEXT = "TLS";
+	private static final String[] ENABLED_PROTOCOLS = new String[] { "TLSv1.2" };
 
-        	//SSLSocketFactory sslsocketfactory = sslContext.getSocketFactory(); 
-        	SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 9999);
-        	sslsocket.startHandshake();
-        	
-            InputStream inputstream = System.in;
-            InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-            BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+	public static void main(String[] arstring) {
+		try {
 
-            OutputStream outputstream = sslsocket.getOutputStream();
-            OutputStreamWriter outputstreamwriter = new OutputStreamWriter(outputstream);
-            BufferedWriter bufferedwriter = new BufferedWriter(outputstreamwriter);
+			
+			// Download http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
+/*			try {
+				Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
+				field.setAccessible(true);
+				field.set(null, java.lang.Boolean.FALSE);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}*/
 
-            String string = null;
-            while ((string = bufferedreader.readLine()) != null) {
-                bufferedwriter.write(string + '\n');
-                bufferedwriter.flush();
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    }
+			
+			SSLContext sslContext = getSslContext();
+			
+			SSLSocketFactory sslsocketfactory = sslContext.getSocketFactory();
+			SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 9999);
+			sslsocket.setEnabledProtocols(ENABLED_PROTOCOLS);
+			System.out.println("Shaking hands");
+			sslsocket.startHandshake();
+
+			InputStream inputstream = System.in;
+			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
+			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+
+			OutputStream outputstream = sslsocket.getOutputStream();
+			OutputStreamWriter outputstreamwriter = new OutputStreamWriter(outputstream);
+			BufferedWriter bufferedwriter = new BufferedWriter(outputstreamwriter);
+
+			String string = null;
+			while ((string = bufferedreader.readLine()) != null) {
+				bufferedwriter.write(string + '\n');
+				bufferedwriter.flush();
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static SSLContext getSslContext() throws KeyStoreException, IOException, NoSuchAlgorithmException,
+			CertificateException, FileNotFoundException, UnrecoverableKeyException, KeyManagementException {
+		KeyStore keyStore = KeyStore.getInstance(JAVA_KEYSTORE_INSTANCE_KEY);
+		keyStore.load(new FileInputStream(JKS_PATH), KEYSTORE_PASSWORD.toCharArray());
+
+		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		keyManagerFactory.init(keyStore, KEYSTORE_PASSWORD.toCharArray());
+		KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+		
+		TrustManagerFactory trustManagerFactory = TrustManagerFactory
+				.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		trustManagerFactory.init(keyStore);
+		//TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+		TrustManager[] trustManagers = new TrustManager[]{new AllTrustingTrustManager()};
+		
+		SSLContext sslContext = SSLContext.getInstance(SSL_CONTEXT);
+		sslContext.init(keyManagers, trustManagers, null);
+		return sslContext;
+	}
 }
--- src/main/java/echoClientServer/EchoServer.java
+++ src/main/java/echoClientServer/EchoServer.java
@@ -1,5 +1,20 @@
 package echoClientServer;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
@@ -8,62 +23,55 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.KeyStore;
-import java.security.Provider;
-import java.security.Security;
-
 public class EchoServer {
+	private static final String[] ENABLED_PROTOCOLS = new String[] { "TLSv1.2" };
+
+	// node private key
+	// openssl genrsa -aes256 -out nodeName_https.key 2048
+	// certificate authority
+	// openssl genrsa -aes256 -out CA.key 2048
+	// certificate authority certificate signing request
+	// openssl req -new -x509 -days 1095 -key CA.key -out CA.crt
+	// node certificate signing request
+	// openssl req -new -x509 -days 365 -key nodeName_https.key -out nodeName_https.crt
+	// node key signing request
+	// openssl req -new -key nodeName_https.key -out nodeName_https.csr
+	// node https certificate signed by CA
+	// openssl x509 -req -days 365 -in nodeName_https.csr -CA CA.crt
+	// -CAkey CA.key -set_serial 01 -out nodeName_csnmt-signed.crt
+	// import CA to java keystore
+	// keytool -import -alias csnmt -file CA.crt -keypass test -keystore nodeName.jks -storepass keystorePassword
+	// import to java keystore
+	// keytool -import -alias nodeName_csnmt-signed -file
+	// nodeName_csnmt-signed.crt -keypass test -keystore nodeName.jks
+	// -storepass keystorePassword
+
+	private static final String JAVA_KEYSTORE_INSTANCE_KEY = "JKS";
+	private static final String JKS_PATH = "keys/keystore.jks";
+	private static final String KEYSTORE_PASSWORD = "keystorePassword";
+	private static final String SSL_CONTEXT = "TLS";
+
 	public static void main(String[] arstring) {
 		try {
-			//node private key
-			//openssl genrsa -aes256 -out nodeName_https.key 2048
-			//certificate authority
-			//openssl genrsa -aes256 -out CA.key 2048
-			//certificate authority certificate signing request
-			//openssl req -new -x509 -days 1095 -key CA.key -out CA.crt
-			//node certificate signing request
-			//openssl req -new -x509 -days 365 -key nodeName_https.key -out nodeName_https.crt
-			//node key signing request
-			//openssl req -new -key nodeName_https.key -out nodeName_https.csr
-			//node https certificate signed by CA
-			//openssl x509 -req -days 365 -in nodeName_https.csr -CA CA.crt -CAkey CA.key -set_serial 01 -out nodeName_csnmt-signed.crt
-			//import CA to java keystore
-			//keytool -import -alias csnmt -file CA.crt -keypass test -keystore nodeName.jks -storepass keystorePassword
-			//import to java keystore
-			//keytool -import -alias nodeName_csnmt-signed -file nodeName_csnmt-signed.crt -keypass test -keystore nodeName.jks -storepass keystorePassword
-			
-			KeyStore keyStore = KeyStore.getInstance("JKS");
-			keyStore.load(new FileInputStream("keys/nodeName.jks"), "keystorePassword".toCharArray());
 
-			for (Provider p : Security.getProviders()){
-				System.out.println(p.getName());
-			}
-			
-			//KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
-			KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-			keyManagerFactory.init(keyStore, "keystorePassword".toCharArray());
+			// Download http://www.oracle.com/technetwork/java/javase/downloads/jce8-download-2133166.html
+/*			try {
+				Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
+				field.setAccessible(true);
+				field.set(null, java.lang.Boolean.FALSE);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}*/
 
-			//KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
-			TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()); 
-			trustManagerFactory.init(keyStore);
+			SSLContext sslContext = getSslContext();
 
-			SSLContext sslContext = SSLContext.getInstance("TLS"); 
-			TrustManager[] trustManagers = trustManagerFactory.getTrustManagers(); 
-			sslContext.init(keyManagerFactory.getKeyManagers(), trustManagers, null); 
+			SSLServerSocketFactory sslServerSocketfactory = sslContext.getServerSocketFactory();
+			SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketfactory.createServerSocket(9999);
+			sslServerSocket.setEnabledProtocols(ENABLED_PROTOCOLS);
+			System.out.println("Listening");
+			SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
 
-			SSLServerSocketFactory sslserversocketfactory = sslContext.getServerSocketFactory(); 
-			SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(9999);
-			SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
-			
-//			SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-//			SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(9999);
-//			SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
-
-			InputStream inputstream = sslsocket.getInputStream();
+			InputStream inputstream = sslSocket.getInputStream();
 			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
 			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
 
@@ -75,5 +83,25 @@ public class EchoServer {
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+	}
+
+	private static SSLContext getSslContext() throws KeyStoreException, IOException, NoSuchAlgorithmException,
+			CertificateException, FileNotFoundException, UnrecoverableKeyException, KeyManagementException {
+		KeyStore keyStore = KeyStore.getInstance(JAVA_KEYSTORE_INSTANCE_KEY);
+		keyStore.load(new FileInputStream(JKS_PATH), KEYSTORE_PASSWORD.toCharArray());
+
+		KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		keyManagerFactory.init(keyStore, KEYSTORE_PASSWORD.toCharArray());
+
+		TrustManagerFactory trustManagerFactory = TrustManagerFactory
+				.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		trustManagerFactory.init(keyStore);
+
+		SSLContext sslContext = SSLContext.getInstance(SSL_CONTEXT);
+		//TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+		TrustManager[] trustManagers = new TrustManager[]{new AllTrustingTrustManager()};
+		KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+		sslContext.init(keyManagers, trustManagers, null);
+		return sslContext;
 	}
 }
