b025ecd413ce7ada22f5a3d8fff2d37743355388
Jonas Eckerström <jonas.eckerstrom@softhouse.se>
2015-10-03 21:51:26 +0200

    More tests

--- src/test/java/sslContextBuilder/SSLContextBuilderTest.java
+++ src/test/java/sslContextBuilder/SSLContextBuilderTest.java
@@ -3,13 +3,13 @@ package sslContextBuilder;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLSocket;
 
@@ -24,24 +24,39 @@ import com.tngtech.java.junit.dataprovider.UseDataProvider;
 @RunWith(DataProviderRunner.class)
 public class SSLContextBuilderTest {
 
+	@Test(expected = SSLHandshakeException.class)
+	public void test_can_fail_to_connect_to_real_ca_without_certificate_chain() throws Exception {
+		SSLContextBuilder.builder().socketBuilder().withHost("www.google.com").socket();
+	}
+
+	@Test
+	public void test_can_connect_to_real_ca() throws Exception {
+		SSLContextBuilder.builder().withJavaCaCertsFile().socketBuilder().withHost("www.google.com").socket();
+	}
+
 	@DataProvider
 	public static Object[][] clientServerBuilderCombinationsProvider() throws Exception {
 		return new Object[][] { //
 				{ "Self signed server key with NonValidatingTrustManager client",
 						SSLContextBuilder.builder().withSelfSignedKeyAndCert("RSA", 2048).build(),
-						SSLContextBuilder.builder().withNonvalidatingTrustManager() }, };
+						SSLContextBuilder.builder().withNonvalidatingTrustManager() },
+				{ "Server and client from same keystore",
+						SSLContextBuilder.builder().withKeystoreFile("keys/keystore.jks", "keystorePassword"),
+						SSLContextBuilder.builder().withKeystoreFile("keys/keystore.jks", "keystorePassword") }
+				//
+		};
 	}
 
 	@Test
 	@UseDataProvider("clientServerBuilderCombinationsProvider")
 	public void test_client_server_builder_combinations(String description, SSLContextBuilder serverBuilder,
 			SSLContextBuilder clientBuilder) throws Exception {
-		SSLServerSocket serverSocket = serverBuilder.socketBuilder().withPort(9999).serverSocket();
+		SSLServerSocket serverSocket = serverBuilder.socketBuilder().withPort(0).serverSocket();
 
 		new Thread(new ServerRunnable(serverSocket)).start();
 
 		SSLSocket sslsocket = SSLContextBuilder.builder().withNonvalidatingTrustManager().socketBuilder()
-				.withHost("localhost").withPort(9999).socket();
+				.withHost("localhost").withPort(serverSocket.getLocalPort()).socket();
 
 		OutputStream outputstream = sslsocket.getOutputStream();
 		OutputStreamWriter outputstreamwriter = new OutputStreamWriter(outputstream);
@@ -64,15 +79,10 @@ public class SSLContextBuilderTest {
 			Socket sslSocket;
 			try {
 				sslSocket = myServerSocket.accept();
-				InputStream inputstream = sslSocket.getInputStream();
-				InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-				BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
-
-				String string = null;
-				while ((string = bufferedreader.readLine()) != null) {
-					System.out.println(string);
-					System.out.flush();
+				BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+				while ((bufferedreader.readLine()) != null) {
 				}
+				sslSocket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 				Assert.fail();
